7909a5b77dad6db2b21619a382136db98d5972dd
Luda <ldcg1@hotmail.com>
2015-03-13 21:50:04 -0430

    Collection and composite view added, template modified with <tr>

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 var ContactManager = new Marionette.Application();
 
 ContactManager.addRegions({
-	mainRegion: "#main-region"
+  mainRegion: "#main-region"
 });
 
+ContactManager.on("initialize:after", function(){
+  ContactManager.ContactsApp.List.Controller.listContacts();
+});
---
+++ assets/js/apps/contacts/list/list_view.js
@@ -0,0 +1,12 @@
+ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
+  List.Contact = Marionette.ItemView.extend({
+    tagName: "tr",
+    template: "#contact-list-item"
+  });
+
+  List.Contacts = Marionette.CollectionView.extend({
+    ttagName: "table",
+	className: "table table-hover",
+	itemView: List.Contact
+  });
+});
