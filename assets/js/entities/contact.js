c77ef3f2c846479956ca8adb965520bd5605be41
Luda <ldcg1@hotmail.com>
2015-03-30 21:46:17 -0430

    Entities in contact collection changed to localstore

--- assets/js/entities/contact.js
+++ assets/js/entities/contact.js
@@ -1,10 +1,23 @@
 ContactManager.module("Entities", function(Entities, ContactManager, Backbone, Marionette, $, _){
-  Entities.Contact = Backbone.Model.extend({});
+  	Entities.Contact = Backbone.Model.extend({
+		urlRoot: "contacts"
+	});
 
-  Entities.ContactCollection = Backbone.Collection.extend({
-    model: Entities.Contact,
-    comparator: "firstName"
-  });
+  	Entities.configureStorage(Entities.Contact);
+
+	Entities.ContactCollection = Backbone.Collection.extend({
+		url: "contacts",
+		model: Entities.Contact,
+		comparator: "firstName"
+	});
+
+	Entities.configureStorage(Entities.ContactCollection);
+
+	getContactEntities: function(){
+		var contacts = new Entities.ContactCollection();
+		contacts.fetch();
+		return contacts;
+	}
 
   var contacts;
 
@@ -14,16 +27,13 @@ ContactManager.module("Entities", function(Entities, ContactManager, Backbone, M
       { id: 2, firstName: "Bob", lastName: "Brigham", phoneNumber: "555-0163" },
       { id: 3, firstName: "Charlie", lastName: "Campbell", phoneNumber: "555-0129" }
     ]);
+    contacts.forEach(function(contact){
+		contact.save();
+	});
+	return contacts;
   };
 
-  var API = {
-    getContactEntities: function(){
-      if(contacts === undefined){
-        initializeContacts();
-      }
-      return contacts;
-    }
-  };
+
 
   ContactManager.reqres.setHandler("contact:entities", function(){
     return API.getContactEntities();
